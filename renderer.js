69fbd05b3f22ca3ea0bf77f832796334a12ffe2c
David Demers <daviddemers@Davids-MacBook-Pro-16.local>
2023-12-11 15:08:39 -0500

    Add commuication between main.js, preload.js, & renderer.js

--- renderer.js
+++ renderer.js
@@ -1,2 +1,9 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const pingpong = async () => {
+  const response = await window.versions.ping()
+  console.log(response) // prints out 'pong'
+}
+
+pingpong()
